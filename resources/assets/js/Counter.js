f45024710cdabf61b6544a34e3d723594b1c1230
ricky <ricky3andika@gmail.com>
2015-12-28 06:14:09 +0700

    first commit

---
+++ resources/assets/js/Counter.js
@@ -0,0 +1,22 @@
+
+class Counter {
+
+	 constructor() {
+
+		this.count = 0;
+
+		setInterval(function() {
+
+			this.tick();
+
+		}.bind(this), 1000);
+	}
+
+	tick() {
+		this.count++;
+
+		console.log(this.count);
+	}
+}
+
+export default Counter;
